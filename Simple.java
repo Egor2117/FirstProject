27553d92d3501b957e9598676c4ff36383792fad
Egor2117 <21vs.cherenko.g@pdu.by>
2024-04-10 14:41:27 +0300

    Create Simple.java

---
+++ Simple.java
@@ -0,0 +1,9 @@
+class Simple {
+    public static void main(String args[]) {
+        String surname = "Иванов";
+        String subject = "Математика";
+
+        System.out.println("Фамилия: " + surname);
+        System.out.println("Предмет: " + subject);
+    }
+}
